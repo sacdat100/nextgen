833e2b76ee8a24b6592899dc2799c9be3f314357
sacdat100 <sacdat100@hotmail.com>
2017-12-26 09:48:52 +0800

    Initial Setup

---
+++ src/client/app/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+  render () {
+    return <p>Next Gen coming soon...</p>;
+  }
+}
+
+render(<App/>, document.getElementById('app'));
