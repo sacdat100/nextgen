b68e9a5c6298c6f1622253a57736592ca5dd0813
sacdat100 <sacdat100@hotmail.com>
2018-01-01 22:00:08 +0800

    add webpack-dev-server support

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
@@ -8,14 +9,25 @@ var config = {
   entry: APP_DIR + '/index.jsx',
   output: {
     path: BUILD_DIR,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/public/'
   },
+  plugins: [
+      //new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin()
+  ],
   module : {
     loaders : [
       {
         test : /\.jsx?/,
+        exclude: /node_modules/,
         include : APP_DIR,
-        loader : 'babel-loader'
+        loader : 'babel-loader',
+        query: {
+            presets: ['stage-3', 'react', 'react-hmre' ],
+            //presets: ['stage-3', 'react' ],
+        }
       }
     ]
   }
